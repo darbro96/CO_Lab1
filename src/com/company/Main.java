f16d1861b942f130fe9376f733c5155a71fa60fc
darbro96 <dbronikowsk@gmail.com>
2021-03-06 17:45:05 +0100

    Dodanie nazwy projektu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
     System.out.println(System.getProperty("java.runtime.version"));
 	System.out.println(System.getProperty("java.version"));
+        System.out.println("Projekt: Laboratorium");
     }
 }
